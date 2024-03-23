bb485284f7fa95bfc0e720419bd96bbcf6e522b2
washka17 <289zxc@gmail.com>
2024-03-23 13:35:54 +0300

    fix problem related to the transition from Laravel 10 version to Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
